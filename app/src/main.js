534a313360b7ef6d863b88b321108ac8c26307c7
hirocaster <hohtsuka@gmail.com>
2015-12-22 17:04:42 +0900

    Install babel

--- app/src/main.js
+++ app/src/main.js
@@ -1,4 +1,5 @@
 'use strict';
+
 const electron = require('electron');
 const app = electron.app;  // Module to control application life.
 const BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
@@ -26,7 +27,7 @@ app.on('ready', function() {
   mainWindow = new BrowserWindow({width: 800, height: 600});
 
   // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
+  mainWindow.loadURL(`file://${__dirname}/../index.html`);
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
